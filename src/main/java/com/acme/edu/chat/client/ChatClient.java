0a03ccd211e8dfdae2c6b49af458412313f6d6f3
Anya <boanya5@mail.ru>
2017-09-07 17:46:56 +0300

    add id identity

--- src/main/java/com/acme/edu/chat/client/ChatClient.java
+++ src/main/java/com/acme/edu/chat/client/ChatClient.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+import static com.acme.edu.chat.Commands.CHANGE_ID_COMMAND;
+
 class ChatClient {
     private String host;
     private Integer port;
@@ -22,6 +24,10 @@ class ChatClient {
              DataInputStream input = new DataInputStream(socket.getInputStream());
              BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in))
         ) {
+            System.out.println("Enter your nickname with command " + CHANGE_ID_COMMAND);
+            String nickname = consoleInput.readLine();
+            output.writeUTF(nickname);
+
             Thread receiver = new Thread(new ChatReceiver(input));
             receiver.setDaemon(true);
             receiver.start();
