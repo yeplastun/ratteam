4e8c9d67b58256d4013b391ba7b0032f92caa3e5
Egor <yeplastun@gmail.com>
2017-09-08 11:16:26 +0300

    added file saving

--- src/main/java/com/acme/edu/chat/server/HistorySaver.java
+++ src/main/java/com/acme/edu/chat/server/HistorySaver.java
@@ -4,14 +4,13 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
 
 import static java.lang.System.lineSeparator;
 
@@ -22,21 +21,23 @@ public class HistorySaver {
     private BufferedReader reader;
     private BufferedWriter writer;
     private HistorySaver() {
-        boolean fileExists = false;
+        boolean isFileExists = false;
         try {
             final File file = new File(filename);
-            fileExists = file.createNewFile();
+            if (!file.exists()) {
+                isFileExists = file.createNewFile();
+            }
             reader = new BufferedReader(
-                    new InputStreamReader(new FileInputStream(filename))
+                    new InputStreamReader(new FileInputStream(filename), Charset.forName("UTF-8"))
             );
             writer = new BufferedWriter(
-                    new OutputStreamWriter(new FileOutputStream(filename))
+                    new OutputStreamWriter(new FileOutputStream(filename), Charset.forName("UTF-8"))
             );
             System.out.println("Logging to: " + file.getAbsolutePath());
         } catch (IOException e) {
             reader = null;
             writer = null;
-            if (fileExists) {
+            if (isFileExists) {
                 System.out.println("Unable to write to created history file.");
 
             } else {
@@ -47,30 +48,25 @@ public class HistorySaver {
     static HistorySaver getInstance() {
         return INSTANCE;
     }
-    public synchronized void addToFile(Message message) throws IOException {
+
+    public void addToFile(Message message) throws IOException {
         if (writer != null) {
             writer.write(message.toString() + lineSeparator());
             writer.flush();
         }
     }
 
-    public List<Message> loadHistory() throws FileNotFoundException {
+    public List<Message> loadHistory() {
         List<Message> messages = new LinkedList<>();
 
         if (reader != null) {
-            String message;
+            String message = "";
             try {
-                while ((message = reader.readLine()) != null && Objects.equals(message, "")) {
+                while ((message = reader.readLine()) != null) {
                     messages.add(Message.fromString(message));
                 }
             } catch (IOException e) {
                 return messages;
-            }
-
-            if (reader != null) {
-                reader = new BufferedReader(
-                        new InputStreamReader(new FileInputStream(filename))
-                );
             }
         }
         return messages;
